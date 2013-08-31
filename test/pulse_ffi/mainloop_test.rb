e94d2001d2d1e1a50fcdbc88970417c88b3ab437
Dimitri Merejkowsky <d.merej@gmail.com>
2013-08-31 13:14:51 +0200

    TDDing Mainloop class

---
+++ test/pulse_ffi/mainloop_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require 'pulse_ffi/mainloop'
+require 'ostruct'
+
+
+module PulseFFI
+  class MainloopTest < MiniTest::Unit::TestCase
+    def test_initialize_creates_mainloop
+      api = OpenStruct.new(pa_mainloop_new: :mainloop_ptr)
+
+      loop = Mainloop.new(api: api)
+      assert_equal :mainloop_ptr, loop.pointer
+    end
+
+  end
+
+end
